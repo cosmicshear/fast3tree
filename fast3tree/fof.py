8812ee1068ed23ed18f8a064de7554255db1eede
Erfan Nourbakhsh <erfanxyz@gmail.com>
2020-05-11 23:44:44 -0700

    display not necessary

--- fast3tree/fof.py
+++ fast3tree/fof.py
@@ -9,15 +9,15 @@ try: # https://github.com/tqdm/tqdm/issues/506
         from tqdm.notebook import tqdm
         # https://github.com/bstriner/keras-tqdm/issues/21
         # this removes the vertical whitespace that remains when tqdm progressbar disappears
-        from IPython.core.display import HTML, display
-        display(HTML("""
+        from IPython.core.display import HTML
+        HTML("""
         <style>
         .p-Widget.jp-OutputPrompt.jp-OutputArea-prompt:empty {
           padding: 0;
           border: 0;
         }
         </style>
-        """))
+        """)
     if 'terminal' in ipy_str:  # ipython
         from tqdm import tqdm 
 except:                        # terminal
