b163e7b4e494615ad463fbcee0a696e05022d9df
Erfan Nourbakhsh <erfanxyz@gmail.com>
2020-05-11 20:29:36 -0700

    Add tqdm progressbar
    This adds a progress bar to this function using the tqdm module (https://github.com/tqdm/tqdm). It automatically detects the environment and shows the appropriate progress bar.

--- fast3tree/fof.py
+++ fast3tree/fof.py
@@ -1,15 +1,33 @@
 import numpy as np
 from .core import fast3tree
 
+# tqdm automatically switches to the text-based
+# progress bar if not running in Jupyter
+try: # https://github.com/tqdm/tqdm/issues/506
+    ipy_str = str(type(get_ipython()))
+    if 'zmqshell' in ipy_str:  # jupyter
+        from tqdm.notebook import tqdm
+    if 'terminal' in ipy_str:  # ipython
+        from tqdm import tqdm 
+except:                        # terminal
+    if sys.stderr.isatty():
+        from tqdm import tqdm
+    else:
+        def tqdm(iterable, **kwargs):
+            return iterable
+
 __all__ = ['find_friends_of_friends']
 
 def find_friends_of_friends(points, linking_length, periodic_box_size=None,
-                            reassign_group_indices=True):
+                            reassign_group_indices=True, **tqdm_kwargs):
+    if tqdm_kwargs is None:
+        tqdm_kwargs={}
+    tqdm_kwargs.setdefault('desc', 'FoF matching');
     group_ids = np.repeat(-1, len(points))
     with fast3tree(points) as tree:
         if periodic_box_size:
             tree.set_boundaries(0, periodic_box_size)
-        for i, point in enumerate(points):
+        for i, point in enumerate(tqdm(points,**tqdm_kwargs)):
             idx = tree.query_radius(point, linking_length, bool(periodic_box_size))
             group_ids_this = np.unique(group_ids[idx])
             group_ids_this = group_ids_this[group_ids_this != -1]
